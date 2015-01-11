0a036378564c651ccb839e9cd10dae9a4faa7cba
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2015-01-11 09:22:35 +0100

    Add Azure worker command

--- TvShowReminderWorker/Program.cs
+++ TvShowReminderWorker/Program.cs
@@ -7,6 +7,9 @@ namespace TvShowReminderWorker
         static void Main(string[] args)
         {
             Console.WriteLine("I am the web job, I ran at {0}", DateTime.Now);
+
+            JobHost host = new JobHost();
+            host.RunAndBlock();
         }
     }
 }
