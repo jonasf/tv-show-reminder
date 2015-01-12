7df01a188884af55a9bd638b682f676f07d18b61
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2015-01-12 22:15:25 +0100

    Add logging lib

--- TvShowReminder/Global.asax.cs
+++ TvShowReminder/Global.asax.cs
@@ -1,19 +1,31 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Web.Mvc;
 using System.Web.Routing;
+using NLog;
 using TvShowReminder.DatabaseMigrations;
 
 namespace TvShowReminder
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
+            Logger.Info("Starting application");
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             ISqlDatabaseMigrator sqlDatabaseMigrator = new SqlDatabaseMigrator();
             sqlDatabaseMigrator.MigrateToLatest(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            Logger.Info("Application startup complete");
+        }
+
+        protected void Application_Error()
+        {
+            Exception lastException = Server.GetLastError();
+            Logger.Fatal(lastException);
         }
     }
 }
