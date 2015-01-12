934ce507d7e7b24823257906695d2e347ea63825
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2015-01-12 21:28:28 +0100

    Add database migration component with subscription table

---
+++ TvShowReminder.DatabaseMigrations/SqlDatabaseMigrator.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+
+namespace TvShowReminder.DatabaseMigrations
+{
+    public class SqlDatabaseMigrator : ISqlDatabaseMigrator
+    {
+        public void MigrateToLatest(string connectionString)
+        {
+            var announcer = new TextWriterAnnouncer(s => System.Diagnostics.Debug.WriteLine(s));
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var migrationContext = new RunnerContext(announcer)
+            {
+                Target = assembly.FullName
+            };
+
+            var options = new MigrationOptions { PreviewOnly = false, Timeout = 60 };
+            var factory = new FluentMigrator.Runner.Processors.SqlServer.SqlServer2014ProcessorFactory();
+            var processor = factory.Create(connectionString, announcer, options);
+            var runner = new MigrationRunner(assembly, migrationContext, processor);
+            runner.MigrateUp(true);
+        }
+        
+        private class MigrationOptions : IMigrationProcessorOptions
+        {
+            public bool PreviewOnly { get; set; }
+            public int Timeout { get; set; }
+            public string ProviderSwitches { get; private set; }
+        }
+    }
+}
