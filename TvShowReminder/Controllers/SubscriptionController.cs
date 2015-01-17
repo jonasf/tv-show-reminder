444643e9d0adfa798d309e75eb54b2b775fd8a58
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2015-01-17 10:52:39 +0100

    Add form for search

--- TvShowReminder/Controllers/SubscriptionController.cs
+++ TvShowReminder/Controllers/SubscriptionController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using TvShowReminder.Model.Query;
+using TvShowReminder.Models;
 using TvShowReminder.Service;
 
 namespace TvShowReminder.Controllers
@@ -15,8 +17,23 @@ namespace TvShowReminder.Controllers
 
         public ActionResult Search(string q)
         {
-            var result = _subscriptionQueryService.Search(new SearchTvShowQuery { Query = q });
-            return View();
+            var searchViewModel = new SearchViewModel();
+
+            if (HasSearchParameters(q))
+            {
+                var result = _subscriptionQueryService.Search(new SearchTvShowQuery { Query = q });
+                searchViewModel.SearchWords = q;
+                searchViewModel.HasSearch = true;
+                searchViewModel.TvShows = result.TvShows;
+                searchViewModel.SearchHits = result.TvShows.Count();
+            }
+
+            return View(searchViewModel);
+        }
+
+        private bool HasSearchParameters(string query)
+        {
+            return !string.IsNullOrEmpty(query);
         }
     }
 }
