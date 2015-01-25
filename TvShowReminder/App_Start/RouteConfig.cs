f1d06308874907a1ef105a87795fca87133204e6
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2015-01-25 18:48:23 +0100

    Removing pretty route

--- TvShowReminder/App_Start/RouteConfig.cs
+++ TvShowReminder/App_Start/RouteConfig.cs
@@ -10,12 +10,6 @@ namespace TvShowReminder
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Search",
-                url: "sok",
-                defaults: new { controller = "Subscription", action = "Search", q = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
