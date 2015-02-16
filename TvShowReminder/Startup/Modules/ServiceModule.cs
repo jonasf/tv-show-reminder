6a0d4ada5f22574c7bd9930eecc66fe5d1c2edea
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2015-02-16 20:51:46 +0100

    Remove deprecated service

--- TvShowReminder/Startup/Modules/ServiceModule.cs
+++ TvShowReminder/Startup/Modules/ServiceModule.cs
@@ -15,7 +15,6 @@ namespace TvShowReminder.Startup.Modules
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<TvRageService>().As<ITvRageService>();
-            builder.RegisterType<SubscriptionQueryService>().As<ISubscriptionQueryService>();
             builder.RegisterType<EpisodesCommandService>().As<IEpisodesCommandService>();
 
             builder.RegisterType<DeleteEpisodesCommandHandler>().As<ICommandHandler<DeleteEpisodesCommand>>();
