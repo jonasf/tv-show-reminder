a4055be18841eb5c0cbf6899a7f7892130082836
jonasf <jonas_fredriksson@hotmail.com>
2015-09-24 22:02:27 +0200

    Add functionality to get data from TvMaze API

---
+++ TvShowReminder.TvMazeApi/Domain/TvMazeShow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TvShowReminder.TvMazeApi.Domain
+{
+    public class TvMazeShow
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Premiered { get; set; }
+        public string Url { get; set; }
+        public TvMazeShowImage Image { get; set; }
+    }
+}
